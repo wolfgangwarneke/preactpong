707cfdea2bda78fa8df40e9265b6ef50e2769eae
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2019-02-08 23:10:11 -0800

    Scaffold Preact project to display simple message

---
+++ app.js
@@ -0,0 +1,10 @@
+import { h, render, Component } from 'preact';
+
+render((
+  <div id="pong-app">
+      <h1>Preact Pong</h1>
+  </div>
+), document.getElementById('app'));
+
+
+console.log('test');
